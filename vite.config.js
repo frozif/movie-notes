bf2e6c819ddd7f5a50c98aaac61e9fe1d9d84704
frozif <smppms1321@gmail.com>
2026-08-06 13:22:50 +0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,17 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({mode}) =>{
+  const isProd = mode ==='production'
+return {
+  base: isProd ? '/movie-notes/' : '/',
+    plugins: [react()],
   resolve: {
   alias: {
   '@': path.resolve(__dirname, './src')
   }
   }
+}
 })
 
 
